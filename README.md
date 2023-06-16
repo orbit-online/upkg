1e8e255d815e0292a4f1b90ba9dc437370060037
Anders Ingemann <aim@orbit.online>
2023-06-16 15:46:44 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,171 @@
+# μpkg - A minimalist package manager
+
+μpkg is a package manager written in <200 lines of bash with just the bare
+minimum of features.  
+Its primary focus is allowing bash scripts to source dependencies like small
+logging functions or commands that shouldn't be tracked with those scripts.
+
+μpkg supports installation to a local project with a `upkg.json` in its root,
+and global installation for user- or system-wide usage.
+
+## Dependencies
+
+- bash>=v4.4
+- git
+- jq
+
+## Installation
+
+```
+wget -qO- https://raw.githubusercontent.com/orbit-online/upkg/v0.9.0/upkg.sh |\
+(
+  src=$(cat)
+  [[ $(shasum -a 256 <<<"$src") = '3469b2b03f735bbfa4875569fd582aa6ac4a81206e3a4f731536cca8095e10b9  -' ]] || { echo 'shasum mismatch!'; exit 1; }
+  bash -c "set - install -g https://github.com/orbit-online/upkg.git@v0.9.0; $src"
+)
+```
+
+## Usage
+
+```
+μpkg - A minimalist package manager
+Usage:
+  upkg install
+  upkg install -g [remoteurl]user/pkg@<version>
+  upkg uninstall -g user/pkg
+  upkg root ${BASH_SOURCE[0]}
+```
+
+`upkg install` looks for a `upkg.json` in the current directory and upwards and
+recursively installs the specified dependencies (see
+[dependencies](#dependencies)).
+
+`upkg install -g` installs the specified package and version (either a full git
+remote URL or a GitHub user/pkg shorthand) to `/usr/local/lib/upkg` (when root)
+or `$HOME/.local` (when not).
+
+`upkg uninstall -g` uninstalls a globally installed package (must be shorthand)
+and all its commands (see [commands](#commands)).
+
+`upkg root` is a utility function for scripts in order to build `source` paths
+(see [Including dependencies](#including-dependencies)).
+
+## Upgrading packages
+
+When installing a package, μpkg first uninstalls the previous install if it
+exists. This means you can simply run `upkg install` to upgrade all packages
+that have a moving version (like a branchname). Note that it is advisable to
+use commit hashes as versions when publishing something that other packages may
+rely on to avoid bumping past breaking changes.
+
+Note: μpkg performs quite a few pre-flight checks before installing or upgrading
+a package and its dependencies in order to avoid leaving packages in a broken
+state. If it does happen, kindly report a reproduce as an issue.
+
+## Including dependencies
+
+`upkg root` allows you to avoid the entire "where the heck is my script installed"
+detective work. Simply run `upkg root "${BASH_SOURCE[0]}"` to get the root
+install path of your package. You can use this both for sourcing dependencies
+but also executing them:
+
+```
+my_fn() {
+  set -e
+  local pkgroot
+  pkgroot=$(upkg root "${BASH_SOURCE[0]}")
+
+  # shellcheck source=.upkg/orbit-online/records.sh/records.sh
+  source "$pkgroot/.upkg/orbit-online/records.sh/records.sh"
+
+  PATH="$pkgroot/.upkg/.bin:$PATH"
+  command-from-dep "$1"
+}
+
+my_fn "$@"
+```
+
+## upkg.json
+
+The upkg.json has no version, package name, or description.
+There are 3 optional config keys in total:
+
+### dependencies
+
+Dependencies of a package. A dictionary of git cloneable URLs or
+GitHub shorthands as keys and git branches/tags/commits as values.
+
+Dependencies will be installed under `.upkg` next to `upkg.json`.
+
+```
+{
+  "dependencies": {
+    "orbit-online/records.sh": "v0.9.2",
+    "andsens/docopt.sh": "v1.0.0-upkg",
+		"git@github.com:secoya/bitwarden-tools": "master"
+  },
+  ...
+}
+```
+
+### files
+
+List of files the package consists of. An array of paths relative to the
+repository root. Only items listed here or in [commands](#commands) will be
+part of the final package installation. All listed paths _must_ exist.
+
+```
+{
+  ...
+  "files": [
+    "lib/common.sh",
+    "lib/commands.sh"
+  ],
+  ...
+}
+```
+
+### commands
+
+List of commands this package provides. A dictionary of command names as keys
+and paths relative to the repository root. Note that the specified files _must_
+be marked as executable.
+
+```
+{
+  ...
+  "commands": {
+    "parse-spec": "bin/parse.sh",
+    "buildit": "bin/build.sh",
+    "checkit": "tools/check.sh"
+  },
+  ...
+}
+```
+
+When installing globally, the listed commands will be installed as symlinks to
+`/usr/local/bin` (when root) or `$HOME/.local/bin` (when not).  
+`upkg uninstall -g` uninstalls those symlinks (provided they still point at the
+package).
+
+When installing locally, the listed commands will be installed to `/.upkg/.bin`.
+
+## Things that μpkg does not and will not support
+
+- `upkg run command`
+- `upkg add/remove usr/package@version`
+- `~`, `^` or other version specifiers
+- Package version locking
+- Package aliases (i.e. non user namespaced package names)
+
+## Things that μpkg _might_ support in the future
+
+- Installing packages via e.g. raw.githubusercontent.com or the local filesystem
+  to avoid the git dependency (would require `name` to be present in `upkg.json`)
+- Using something like `JSON.sh` to avoid the jq dependency
+- Some kind of index of public packages
+
+## Alternatives
+
+https://github.com/bpkg/bpkg  
+https://github.com/basherpm/basher
---
+++ upkg.sh
@@ -0,0 +1,199 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+shopt -s inherit_errexit
+
+upkg() {
+  [[ ! $(bash --version | head -n1) =~ version\ [34]\.[0-3] ]] || fatal "upkg: upkg requires bash >= v4.4\n"
+  if ! type jq >/dev/null 2>&1 || ! type git >/dev/null 2>&1; then
+    fatal "upkg: Unable to find dependencies 'jq' and 'git'.\n"
+  fi
+  DOC="μpkg - A minimalist package manager
+Usage:
+  upkg install
+  upkg install -g [remoteurl]user/pkg@<version>
+  upkg uninstall -g user/pkg
+  upkg root \${BASH_SOURCE[0]}
+"
+  [[ -n $1 ]] || fatal "$DOC"
+  local subcmd=$1 prefix=$HOME/.local pkgspath tmppkgspath
+  [[ $EUID != 0 ]] || prefix=/usr/local
+  case "$subcmd" in
+    install)
+      tmppkgspath=$(mktemp -d)
+      # shellcheck disable=2064
+      trap "rm -rf \"$tmppkgspath\"" EXIT
+      if [[ $# -eq 3 && $2 = -g ]]; then
+        upkg_prepare_dep "$3" "$prefix/lib/upkg" "$prefix/bin" "$tmppkgspath"
+        upkg_install_dep "$3" "$prefix/lib/upkg" "$prefix/bin" "$tmppkgspath"
+        printf "upkg: Installed %s\n" "$3" >&2
+      elif [[ $# -eq 1 ]]; then
+        pkgspath=$(upkg_root)
+        upkg_prepare_deps "$pkgspath" "$tmppkgspath" "$pkgspath/upkg.json"
+        upkg_install_deps "$pkgspath" "$tmppkgspath"
+        printf "upkg: Installed all dependencies\n" >&2
+      else
+        fatal "$DOC"
+      fi
+      ;;
+    uninstall)
+      if [[ $# -ne 3 && $2 = -g ]]; then
+        [[ $3 =~ ^([^@/: ]+/[^@/: ]+)$ ]] || fatal "upkg: Expected packagename ('user/pkg') not '%s'" "$3"
+        upkg_uninstall_dep "$3" "$prefix/lib/upkg" "$prefix/bin"
+        printf "upkg: Uninstalled %s\n" "$3" >&2
+      else
+        fatal "$DOC"
+      fi
+      ;;
+    root)
+      [[ -n $2 ]] || fatal "$DOC"
+      upkg_root "$2" ;;
+    *) fatal "$DOC" ;;
+  esac
+}
+
+upkg_root() (
+  local sourcing_file=$1
+  if [[ -n $sourcing_file ]]; then
+    cd "$(dirname "$(realpath "${sourcing_file}")")"
+  fi
+  until [[ -e $PWD/upkg.json ]]; do
+    if [[ $PWD = '/' ]]; then
+      fatal 'upkg root: Unable to find package root (no upkg.json found in this or any parent directory)'
+    fi
+    cd ..
+  done
+  printf "%s\n" "$PWD"
+)
+
+upkg_parse_repospec() {
+  if [[ $repospec =~ ^([^@/: ]+/[^@/: ]+)(@([^@ ]+))$ ]]; then
+    printf "https://github.com/%s.git %s %s\n" "${BASH_REMATCH[1]}" "${BASH_REMATCH[1]}" "${BASH_REMATCH[3]}"
+  elif [[ $repospec =~ ([^@/: ]+/[^@/ ]+)(@([^@ ]+))$ ]]; then
+    printf "%s %s %s\n" "${repospec%@*}" "${BASH_REMATCH[1]}" "${BASH_REMATCH[3]}"
+  else
+    fatal "upkg: Unable to parse repospec '%s'. Expected a git cloneable URL followed by @version" "$repospec"
+  fi
+}
+
+upkg_uninstall_dep() {
+  local pkgname=$1 pkgspath=$2 binpath=$3
+  local pkgpath="$pkgspath/$pkgname"
+  # When upgrading remove old files & commands first
+  local file command commands cmdpath
+  if [[ ! -e "$pkgpath/upkg.json" ]]; then
+    fatal "upkg: '%s' is not installed" "$pkgname"
+  fi
+  commands=$(jq -r '(.commands // {}) | to_entries[] | "\(.key)\n\(.value)"' <"$pkgpath/upkg.json")
+  while [[ -n $commands ]] && read -r -d $'\n' command; do
+    read -r -d $'\n' file
+    cmdpath="$binpath/$command"
+    if [[ -e $cmdpath && $(realpath "$cmdpath") = $pkgpath/* ]]; then
+      rm "$cmdpath"
+    fi
+  done <<<"$commands"
+  rm -rf "$pkgpath"
+  if [[ -z $(find "$(dirname "$pkgpath")" -mindepth 1 -maxdepth 1) ]]; then
+    rm -rf "$(dirname "$pkgpath")"
+  fi
+}
+
+upkg_prepare_dep() {
+  local repospec=$1 pkgspath=$2 binpath=$3 tmppkgspath=$4 parsed_spec repourl pkgname pkgversion
+  parsed_spec=$(upkg_parse_repospec "$repospec")
+  read -r -d $'\n' repourl pkgname pkgversion <<<"$parsed_spec"
+  local pkgpath="$pkgspath/$pkgname" tmppkgpath=$tmppkgspath/$pkgname
+  if [[ -n "$pkgversion" ]]; then
+    if ! out=$(git clone -q --depth=1 --branch="$pkgversion" "$repourl" "$tmppkgpath" 2>&1); then
+      fatal "upkg: Unable to clone. Error:\n%s" "$out"
+    fi
+  else
+    if ! out=$(git clone -q --depth=1 "$repourl" "$tmppkgpath" 2>&1); then
+      fatal "upkg: Unable to clone. Error:\n%s" "$out"
+    fi
+  fi
+  if ! jq empty < "$tmppkgpath/upkg.json"; then
+    fatal "upkg: The package '%s' does not contain a valid upkg.json" "$pkgname"
+  fi
+
+  local file files command commands cmdpath
+  files=$(jq -r '(["upkg.json"] + (.files // []) + [(.commands // {})[]] | unique)[]' <"$tmppkgpath/upkg.json")
+  commands=$(jq -r '(.commands // {}) | to_entries[] | "\(.key)\n\(.value)"' <"$tmppkgpath/upkg.json")
+  while [[ -n $files ]] && read -r -d $'\n' file; do
+    if [[ $file =~ ^/ || $file =~ /\.\.(/|$) || $file =~ // || ! -f "$tmppkgpath/$file" ]]; then
+      fatal "upkg: Error on file '%s' in package %s@%s.
+All files in 'files' and 'commands' must:
+* be relative
+* not contain parent dir parts ('../')
+* exist in the repository" "$file" "$pkgname" "$pkgversion"
+    fi
+  done <<<"$files"
+  while [[ -n $commands ]] && read -r -d $'\n' command; do
+    read -r -d $'\n' file
+    if [[ ! -x "$tmppkgpath/$file" ]]; then
+      fatal "upkg: Error on command '%s' in package %s@%s. The file '%s' does not exist or is not executable" "$command" "$file" "$pkgname" "$pkgversion"
+    fi
+    if [[ $command =~ (/| ) ]]; then
+      fatal "upkg: Error on command '%s' in package %s@%s. The command may not contain spaces or slashes" "$command" "$pkgname" "$pkgversion"
+    fi
+    cmdpath="$binpath/$command"
+    if [[ -e $cmdpath && $(realpath "$cmdpath") != $pkgpath/* ]]; then
+      fatal "upkg: Error on command '%s' in package %s@%s. The symlink for it exists and does not point to the package" "$command" "$pkgname" "$pkgversion"
+    fi
+  done <<<"$commands"
+
+  upkg_prepare_deps "$pkgpath" "$tmppkgspath/.upkg" "$tmppkgpath/upkg.json"
+}
+
+upkg_install_dep() {
+  local repospec=$1 pkgspath=$2 binpath=$3 tmppkgspath=$4 parsed_spec _repourl pkgname _pkgversion
+  parsed_spec=$(upkg_parse_repospec "$repospec")
+  read -r -d $'\n' _repourl pkgname _pkgversion <<<"$parsed_spec"
+  local pkgpath="$pkgspath/$pkgname" tmppkgpath=$tmppkgspath/$pkgname
+
+  if [[ -e "$pkgpath" ]]; then
+    upkg_uninstall_dep "$pkgname" "$pkgspath" "$binpath"
+  fi
+
+  local file files command commands
+  files=$(jq -r '(["upkg.json"] + (.files // []) + [(.commands // {})[]] | unique)[]' <"$tmppkgpath/upkg.json")
+  commands=$(jq -r '(.commands // {}) | to_entries[] | "\(.key)\n\(.value)"' <"$tmppkgpath/upkg.json")
+  while [[ -n $files ]] && read -r -d $'\n' file; do
+    mkdir -p "$(dirname "$pkgpath/$file")"
+    cp -a "$tmppkgpath/$file" "$pkgpath/$file"
+  done <<<"$files"
+  if [[ -n $commands ]]; then
+    mkdir -p "$binpath"
+    while [[ -n $commands ]] && read -r -d $'\n' command; do
+      read -r -d $'\n' file
+      ln -s "$pkgpath/$file" "$binpath/$command"
+    done <<<"$commands"
+  fi
+
+  upkg_install_deps "$pkgpath" "$tmppkgspath/.upkg"
+}
+
+upkg_prepare_deps() {
+  local pkgpath=$1 tmppkgspath=$2 tmpkupkgpath=$3 deps dep
+  deps=$(jq -r '(.dependencies // []) | to_entries[] | "\(.key)@\(.value)"' <"$tmpkupkgpath")
+  while [[ -n $deps ]] && read -r -d $'\n' dep; do
+    upkg_prepare_dep "$dep" "$pkgpath/.upkg" "$pkgpath/.upkg/.bin" "$tmppkgspath/.upkg" || fatal "upkg: Error while installing deps for '%s'" "$dep"
+  done <<<"$deps"
+}
+
+upkg_install_deps() {
+  local pkgpath=$1 tmppkgspath=$2 deps dep
+  deps=$(jq -r '(.dependencies // []) | to_entries[] | "\(.key)@\(.value)"' <"$pkgpath/upkg.json")
+  while [[ -n $deps ]] && read -r -d $'\n' dep; do
+    upkg_install_dep "$dep" "$pkgpath/.upkg" "$pkgpath/.upkg/.bin" "$tmppkgspath/.upkg" || fatal "upkg: Error while installing deps for '%s'" "$pkgpath/upkg.json"
+  done <<<"$deps"
+}
+
+fatal() {
+  local tpl=$1; shift
+  # shellcheck disable=2059
+  printf -- "$tpl\n" "$@" >&2
+  exit 1
+}
+
+upkg "$@"
