d02b8ab4f995a713a1652b9f22f179fdb3ffef4f
Anders Ingemann <aim@orbit.online>
2023-08-02 12:33:42 +0200

    Improve testing script

--- test.sh
+++ test.sh
@@ -13,13 +13,13 @@ RUN <<EOR
 set -e
 apt-get update
 apt-get install -y --no-install-recommends wget ca-certificates git ssh jq tree
-ln -s /usr/local/upkg/upkg/upkg.sh /usr/local/bin/upkg
+ln -s ../lib/upkg/orbit-online/upkg/upkg.sh /usr/local/bin/upkg
 EOR
 
 ENV SSH_AUTH_SOCK=/ssh_auth
 EOD
 )
-  docker run --rm -ti -v"$PKGROOT:/usr/local/upkg/upkg" -v"${SSH_AUTH_SOCK}:/ssh_auth" "$sha"
+  docker run --rm -ti -v"$PKGROOT:/usr/local/lib/upkg/orbit-online/upkg" -v"${SSH_AUTH_SOCK}:/ssh_auth" "$sha"
 }
 
 main "$@"
