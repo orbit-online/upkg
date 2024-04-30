caec6efdec8b1c609d08e98148d470321b116904
Anders Ingemann <aim@orbit.online>
2024-04-30 14:55:06 +0200

    Implement containerized test runner

---
+++ tests/run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -Eeo pipefail
+PKGROOT=$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..")
+
+main() {
+  (docker buildx build -q "$PKGROOT" --tag upkg-tests --file - <<EOD
+FROM debian
+SHELL ["/bin/bash", "-Eeo", "pipefail", "-c"]
+
+RUN apt-get update && apt-get install -y --no-install-recommends \\
+  wget ca-certificates git ssh jq tree netcat-openbsd bsdextrautils diffutils
+RUN wget -qO /tmp/delta.deb https://github.com/dandavison/delta/releases/download/0.17.0/git-delta_0.17.0_amd64.deb; \\
+  dpkg -i /tmp/delta.deb
+
+WORKDIR /usr/local/bats
+RUN <<EOINSTALL
+wget -qO- https://github.com/bats-core/bats-core/archive/refs/tags/v1.11.0.tar.gz | tar xz --strip-components 1
+./install.sh /usr/local
+cd lib
+mkdir bats-support bats-assert bats-file
+wget -qO- https://github.com/bats-core/bats-support/archive/refs/tags/v0.3.0.tar.gz | tar xzC bats-support --strip-components 1
+wget -qO- https://github.com/bats-core/bats-assert/archive/refs/tags/v2.1.0.tar.gz | tar xzC bats-assert --strip-components 1
+wget -qO- https://github.com/bats-core/bats-file/archive/refs/tags/v0.4.0.tar.gz | tar xzC bats-file --strip-components 1
+EOINSTALL
+
+ENV BATS_LIB_PATH=/usr/local/bats/lib
+
+WORKDIR /upkg
+ENTRYPOINT ["/usr/local/bin/bats"]
+CMD ["tests"]
+EOD
+  ) >/dev/null
+  exec docker run --rm -ti -eUPDATE_SNAPSHOTS -v"$PKGROOT:/upkg:ro" upkg-tests "$@"
+}
+
+main "$@"
