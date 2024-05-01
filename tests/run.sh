233a2b56f9697adf6c92f048ee21dcd1b4bc5524
Anders Ingemann <aim@orbit.online>
2024-05-01 13:18:52 +0200

    Use single Dockerfile for tests and sandbox

--- tests/run.sh
+++ tests/run.sh
@@ -5,35 +5,7 @@ PKGROOT=$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..")
 
 main() {
   local shasum
-  shasum=$(docker buildx build -q "$PKGROOT" --tag upkg-tests --file - <<EOD
-FROM ubuntu:22.04
-SHELL ["/bin/bash", "-Eeo", "pipefail", "-c"]
-
-RUN apt-get update && apt-get install -y --no-install-recommends \\
-  wget ca-certificates git ssh jq tree netcat-openbsd bsdextrautils diffutils
-RUN wget -qO /tmp/delta.deb https://github.com/dandavison/delta/releases/download/0.17.0/git-delta_0.17.0_amd64.deb; \\
-  dpkg -i /tmp/delta.deb
-
-WORKDIR /usr/local/bats
-RUN <<EOINSTALL
-wget -qO- https://github.com/bats-core/bats-core/archive/refs/tags/v1.11.0.tar.gz | tar xz --strip-components 1
-./install.sh /usr/local
-cd lib
-mkdir bats-support bats-assert bats-file
-wget -qO- https://github.com/bats-core/bats-support/archive/refs/tags/v0.3.0.tar.gz | tar xzC bats-support --strip-components 1
-wget -qO- https://github.com/bats-core/bats-assert/archive/refs/tags/v2.1.0.tar.gz | tar xzC bats-assert --strip-components 1
-wget -qO- https://github.com/bats-core/bats-file/archive/refs/tags/v0.4.0.tar.gz | tar xzC bats-file --strip-components 1
-EOINSTALL
-
-ENV BATS_LIB_PATH=/usr/local/bats/lib
-
-WORKDIR /upkg
-RUN useradd --uid "$UID" -d /upkg "$USER"
-USER "$USER"
-ENTRYPOINT ["/usr/local/bin/bats"]
-CMD ["tests"]
-EOD
-  )
+  shasum=$(docker buildx build -q --file "$PKGROOT/tests/Dockerfile" --build-arg="UID=$UID" --build-arg="USER=$USER" "$PKGROOT")
   local mode=ro docker_opts=()
   ! ${UPDATE_SNAPSHOTS:-false} || mode=rw
   ! ${CREATE_SNAPSHOTS:-false} || mode=rw
@@ -42,7 +14,11 @@ EOD
   else
     docker_opts+=( -a stdout -a stderr)
   fi
-  exec docker run --rm "${docker_opts[@]}" -eUPDATE_SNAPSHOTS -eCREATE_SNAPSHOTS -v"$PKGROOT:/upkg:$mode" "$shasum" "$@"
+  exec docker run --rm "${docker_opts[@]}" \
+    --name upkg-tests \
+    -eUPDATE_SNAPSHOTS -eCREATE_SNAPSHOTS \
+    -v"$PKGROOT:/upkg:$mode" \
+    "$shasum" "$@"
 }
 
 main "$@"
--- tests/sandbox.sh
+++ tests/sandbox.sh
@@ -4,32 +4,17 @@ set -Eeo pipefail
 PKGROOT=$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..")
 
 main() {
-  docker buildx build "$PKGROOT" --tag upkg-sandbox --file - <<EOD
-FROM ubuntu:22.04
-SHELL ["/bin/bash", "-Eeo", "pipefail", "-c"]
-
-RUN apt-get update && apt-get install -y --no-install-recommends \\
-  wget ca-certificates git ssh jq tree netcat-openbsd bsdextrautils diffutils
-RUN wget -qO /tmp/delta.deb https://github.com/dandavison/delta/releases/download/0.17.0/git-delta_0.17.0_amd64.deb; \\
-  dpkg -i /tmp/delta.deb
-
-WORKDIR /upkg/sandbox
-RUN useradd --uid "$UID" -d /upkg/sandbox "$USER"
-RUN adduser "$USER" sudo
-COPY <<EOF /etc/sudoers.d/nopass
-$USER ALL=(ALL) NOPASSWD:ALL
-EOF
-ENV PATH=\$PATH/upkg/bin
-ENV SSH_AUTH_SOCK=/ssh_auth
-EOD
+  local shasum
+  shasum=$(docker buildx build -q --file "$PKGROOT/tests/Dockerfile" --build-arg="UID=$UID" --build-arg="USER=$USER" "$PKGROOT")
   mkdir -p "$PKGROOT/sandbox"
   printf "*" > "$PKGROOT/sandbox/.gitignore"
   docker run --rm -ti \
-    --user "$USER" \
+    --workdir '/upkg/sandbox' \
     -v"$PKGROOT:/upkg:ro" \
     -v"$PKGROOT/sandbox:/upkg/sandbox:rw" \
     -v"${SSH_AUTH_SOCK}:/ssh_auth" \
-    upkg-sandbox
+    --entrypoint /bin/bash \
+    "$shasum"
 }
 
 main "$@"
