32342b6eb67f9e3d39227c7119fd9dade104a29e
Anders Ingemann <aim@orbit.online>
2024-05-01 13:54:31 +0200

    tests: Drop user into existing sandbox if running

--- tests/sandbox.sh
+++ tests/sandbox.sh
@@ -8,13 +8,18 @@ main() {
   shasum=$(docker buildx build -q --file "$PKGROOT/tests/Dockerfile" --build-arg="UID=$UID" --build-arg="USER=$USER" "$PKGROOT")
   mkdir -p "$PKGROOT/sandbox"
   printf "*" > "$PKGROOT/sandbox/.gitignore"
-  docker run --rm -ti --name upkg-sandbox \
-    --workdir '/upkg/sandbox' \
-    -v"$PKGROOT:/upkg:ro" \
-    -v"$PKGROOT/sandbox:/upkg/sandbox:rw" \
-    -v"${SSH_AUTH_SOCK}:/ssh_auth" \
-    --entrypoint /bin/bash \
-    "$shasum"
+  if [[ $(docker container inspect -f '{{.State.Running}}' upkg-sandbox 2>/dev/null) = "true" ]]; then
+    printf "sandbox.sh: upkg-sandbox already running, using docker exec instead\n" >&2
+    exec docker exec -ti upkg-sandbox /bin/bash
+  else
+    exec docker run --rm -ti --name upkg-sandbox \
+      --workdir '/upkg/sandbox' \
+      -v"$PKGROOT:/upkg:ro" \
+      -v"$PKGROOT/sandbox:/upkg/sandbox:rw" \
+      -v"${SSH_AUTH_SOCK}:/ssh_auth" \
+      --entrypoint /bin/bash \
+      "$shasum"
+  fi
 }
 
 main "$@"
