7a46b141f8dddedd21561003de27c575fac6aba4
Anders Ingemann <aim@orbit.online>
2024-05-01 10:48:28 +0200

    Align sandbox with test

--- tests/sandbox.sh
+++ tests/sandbox.sh
@@ -5,18 +5,21 @@ PKGROOT=$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..")
 
 main() {
   docker buildx build "$PKGROOT" --tag upkg-sandbox --file - <<EOD
-FROM debian
+FROM ubuntu:22.04
 SHELL ["/bin/bash", "-Eeo", "pipefail", "-c"]
 
 RUN apt-get update && apt-get install -y --no-install-recommends \\
-  wget ca-certificates git ssh jq tree psmisc sudo
-RUN ln -s /upkg/bin/upkg /usr/local/bin/upkg
+  wget ca-certificates git ssh jq tree netcat-openbsd bsdextrautils diffutils
+RUN wget -qO /tmp/delta.deb https://github.com/dandavison/delta/releases/download/0.17.0/git-delta_0.17.0_amd64.deb; \\
+  dpkg -i /tmp/delta.deb
+
 WORKDIR /upkg/sandbox
 RUN useradd --uid "$UID" -d /upkg/sandbox "$USER"
 RUN adduser "$USER" sudo
 COPY <<EOF /etc/sudoers.d/nopass
 $USER ALL=(ALL) NOPASSWD:ALL
 EOF
+ENV PATH=\$PATH/upkg/bin
 ENV SSH_AUTH_SOCK=/ssh_auth
 EOD
   mkdir -p "$PKGROOT/sandbox"
