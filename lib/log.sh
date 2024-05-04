f2512a48f6bd5812c769b06a5525b9e0cbe43b12
Anders Ingemann <aim@orbit.online>
2024-05-04 12:24:18 +0200

    Add verbose logging and --quiet switch

--- lib/log.sh
+++ lib/log.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2059
+
+verbose() {
+  $VERBOSE || return 0
+  local tpl=$1; shift
+  printf -- "upkg: $tpl\n" "$@" >&2
+}
 
 processing() {
-  ! ${UPKG_SILENT:-false} || return 0
+  ! ${QUIET:-false} || return 0
   local tpl=$1; shift
-  if [[ -t 2 ]]; then
+  if ! $VERBOSE && [[ -t 2 ]]; then
     printf -- "\e[2Kupkg: $tpl\r" "$@" >&2
   else
     printf -- "upkg: $tpl\n" "$@" >&2
@@ -11,9 +18,9 @@ processing() {
 }
 
 warning() {
-  ! ${UPKG_SILENT:-false} || return 0
+  ! ${QUIET:-false} || return 0
   local tpl=$1; shift
-  if [[ -t 2 ]]; then
+  if ! $VERBOSE && [[ -t 2 ]]; then
     printf -- "\e[2Kupkg: $tpl\n" "$@" >&2
   else
     printf -- "upkg: $tpl\n" "$@" >&2
@@ -22,10 +29,18 @@ warning() {
 
 fatal() {
   local tpl=$1; shift
-  if [[ -t 2 ]]; then
+  if ! $VERBOSE && [[ -t 2 ]]; then
     printf -- "\e[2Kupkg: $tpl\n" "$@" >&2
   else
     printf -- "upkg: $tpl\n" "$@" >&2
   fi
   return 1
+}
+
+# Used for adding a newline at the end of execution
+trailing_newline() {
+  ! ${QUIET:-false} || return 0
+  if ! $VERBOSE && [[ -t 2 ]]; then
+    printf "\n"
+  fi
 }
--- lib/utils.sh
+++ lib/utils.sh
@@ -87,28 +87,39 @@ upkg_guess_pkgtype() {
   # shellcheck disable=SC2209
   if [[ $checksum =~ ^[a-z0-9]{40}$ ]]; then
     pkgtype=git
+    verbose "Guessing pkgtype is git, based on 40 hexchars in checksum"
   elif [[ $checksum =~ ^[a-z0-9]{64}$ ]]; then
     if [[ $pkgurl =~ (\.tar(\.[^.?#/]+)?)([?#]|$) ]]; then
       pkgtype=tar
+      verbose "Guessing pkgtype is tar, based on the checksum being 64 hexchars and the URL ending in .tar or .tar.*"
     else
+      verbose "Guessing pkgtype is file, based on the checksum being 64 hexchars and the URL not ending in .tar or .tar.*"
       pkgtype=file
     fi
   elif [[ -e $pkgurl ]]; then
-    if [[ $pkgurl =~ (\.tar(\.[^.?#/]+)?)$ ]]; then
-      pkgtype=tar
-    elif [[ -f $pkgurl ]]; then
-      pkgtype=file
+    if [[ -f $pkgurl ]]; then
+      if [[ $pkgurl =~ (\.tar(\.[^.?#/]+)?)$ ]]; then
+        pkgtype=tar
+        verbose "Guessing pkgtype is tar, based on the URL being a file that exists on the machine and it ending in .tar or .tar.*"
+      else
+        pkgtype=file
+        verbose "Guessing pkgtype is tar, based on the URL being a file that exists on the machine and it not ending in .tar or .tar.*"
+      fi
     elif [[ -d $pkgurl ]] && [[ -e $pkgurl/HEAD || -e $pkgurl/.git/HEAD ]]; then
       pkgtype=git
+      verbose "Guessing pkgtype is git, based on the URL being a directory that exists on the machine and <PATH>/HEAD or <PATH>/.git/HEAD existing"
     else
       fatal "Unable to determine package type from path '%s', please provide -t PKGTYPE" "$pkgurl"
     fi
   elif [[ $pkgurl =~ (\.tar(\.[^.?#/]+)?)([?#]|$) ]]; then
     pkgtype=tar
+    verbose "Guessing pkgtype is tar, based on the URL ending in .tar or .tar.*"
   elif git ls-remote -q "${pkgurl%%'#'*}" HEAD >/dev/null 2>&1; then
     pkgtype=git
+    verbose "Guessing pkgtype is git, based on \`git ls-remote HEAD <URL>\` not returning an error"
   elif upkg_head "$pkgurl"; then
     pkgtype=file
+    verbose "Guessing pkgtype is file, based on wget/curl succeeding in doing a HEAD request to <URL>"
   else
     fatal "Unable to determine package type from URL '%s'" "$pkgurl"
   fi
