0eb2cf814106bb50d8725538fa87e4f1768c55de
Anders Ingemann <aim@orbit.online>
2025-07-31 10:30:19 +0200

    tests: Fix build-container.sh (invoke buildx directly if available)

--- tests/lib/build-container.sh
+++ tests/lib/build-container.sh
@@ -5,9 +5,11 @@ build_container() {
   # * debian:bookworm
   # * rockylinux:8.9
   # * fedora:41
-  # * alpin:3.19
+  # * alpine:3.19
   local baseimg=${BASEIMG:-"ubuntu:22.04"}
   local tag=upkg-testing:${baseimg//:/-}
-  docker buildx build --tag "$tag" --file "$PKGROOT/tests/Dockerfile" --build-arg="BASEIMG=$baseimg" --build-arg="UID=$UID" --build-arg="USER=$USER" "$PKGROOT"
+  local buildx=buildx
+  type buildx &>/dev/null || buildx="docker buildx"
+  $buildx build --tag "$tag" --load --file "$PKGROOT/tests/Dockerfile" --build-arg="BASEIMG=$baseimg" --build-arg="UID=$UID" --build-arg="USER=$USER" "$PKGROOT"
   printf "%s\n" "$tag"
 }
